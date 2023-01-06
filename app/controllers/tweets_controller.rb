32dbb47d4ff4bbf24a4771a3a6f9908ab8dbe1b9
Shahid Farooq <sif101@hotmail.co.uk>
2023-01-06 23:20:49 +0000

    minor changes to tweet controller, added params method and changed tweet index collection

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,20 @@
 class TweetsController < ApplicationController
   def index
     @tweet = Tweet.new
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
   end
 
   def destroy
   end
 
   def edit
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
